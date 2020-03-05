a1285d1db5bb2a20938e64932911964f62a38f21
zzaro <59715304+zzaro@users.noreply.github.com>
2020-03-05 17:07:11 +0900

    RestTemplate으로 수정

--- gachonbot-develop/src/main/java/com/bot/gachon/controller/GachonController.java
+++ gachonbot-develop/src/main/java/com/bot/gachon/controller/GachonController.java
@@ -21,14 +21,12 @@ import java.util.Map;
 @RestController
 public class GachonController {
 
-    @GetMapping("/api")
+    @GetMapping("/dust")
     public String callAPI() throws JsonProcessingException {
 
         HashMap<String, Object> result = new HashMap<String, Object>();
 
         String jsonInString = "";
-
-        try {
             RestTemplate restTemplate = new RestTemplate();
 
             HttpHeaders header = new HttpHeaders();
@@ -37,31 +35,16 @@ public class GachonController {
             String url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst";
             UriComponents uri = UriComponentsBuilder.fromHttpUrl(url+"?"+"serviceKey=%2FFMFpVx%2Bvq%2BeEKK6owg7QDf44oVwyYvRL8I8DAo1ZgHpJmeQ0YXKYlelKIafadOJJ5nB%2B1OV1l5pJyKwMuI3Pg%3D%3D&numOfRows=10&pageNo=1&sidoName=%EC%84%9C%EC%9A%B8&searchCondition=DAILY&_returnType=json").build();
 
-            //이 한줄의 코드로 API를 호출해 MAP타입으로 전달 받는다.
+            //API 호출
             ResponseEntity<Map> resultMap = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Map.class);
-            result.put("statusCode", resultMap.getStatusCodeValue()); //http status code를 확인
+            result.put("statusCode", resultMap.getStatusCodeValue());
             result.put("header", resultMap.getHeaders()); //헤더 정보 확인
             result.put("body", resultMap.getBody()); //실제 데이터 정보 확인
 
-            //데이터를 제대로 전달 받았는지 확인 string형태로 파싱해줌
+            //string형태로 파싱해줌
             ObjectMapper mapper = new ObjectMapper();
             jsonInString = mapper.writeValueAsString(resultMap.getBody());
-
-        } catch (HttpClientErrorException | HttpServerErrorException e) {
-            result.put("statusCode", e.getRawStatusCode());
-            result.put("body"  , e.getStatusText());
-            System.out.println("dfdfdfdf");
-            System.out.println(e.toString());
-
-        } catch (Exception e) {
-            result.put("statusCode", "999");
-            result.put("body"  , "excpetion오류");
-            System.out.println(e.toString());
-        }
-
         return jsonInString;
-
     }
-
 }
 
