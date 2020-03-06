803b2d88a2d11e5ddbc88d5d940fbf5efb3fa02d
Somin <qkrthals0524@naver.com>
2020-03-06 23:04:36 +0900

    #5 오늘의 학식

--- src/main/java/com/bot/gachon/service/Url.java
+++ src/main/java/com/bot/gachon/service/Url.java
@@ -5,5 +5,8 @@ package com.bot.gachon.service;
 public class Url {
 
     public static final String WEATHER_URL =  "http://api.openweathermap.org/data/2.5/weather?id=1897000&APPID=08275712021d8895752cf798fb603cde&lang=kr&units=metric";
-    public static final String HAKSIK_URL = "http://m.gachon.ac.kr/menu/menu.jsp?gubun=C";
+    public  static final String HAKSIK_URL_ART ="http://m.gachon.ac.kr/menu/menu.jsp?gubun=A";
+    public  static final String HAKSIK_URL_EDU ="http://m.gachon.ac.kr/menu/menu.jsp?gubun=B";
+    public static final String HAKSIK_URL_VISION = "http://m.gachon.ac.kr/menu/menu.jsp?gubun=C";
+
 }
