a45f6f399661be848dacd256d0a659075b1f644d
zzaro <59715304+zzaro@users.noreply.github.com>
2020-03-13 12:15:56 +0900

    2번째 수정
    "SERVICE KEY IS NOT REGISTERED ERROR" 오류가 발생하고 있습니다. 
    
    일단 조치해본 사항으로는 공공데이터 API 서비스키는 이미 인코딩되어있기 때문에 decode해서 API를 날리고, URIComponents builder의 자동 인코딩을 막기 위해 build(false)를 넣었습니다.

--- gachonbot-develop/src/main/java/com/bot/gachon/controller/GachonController.java
+++ gachonbot-develop/src/main/java/com/bot/gachon/controller/GachonController.java
@@ -1,50 +1,44 @@
 package com.bot.gachon.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
+
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 
 @RestController
 public class GachonController {
 
     @GetMapping("/dust")
-    public String callAPI() throws JsonProcessingException {
-
-        HashMap<String, Object> result = new HashMap<String, Object>();
-
-        String jsonInString = "";
+    public Object callAPI() throws JsonProcessingException, UnsupportedEncodingException {
             RestTemplate restTemplate = new RestTemplate();
-
             HttpHeaders header = new HttpHeaders();
             HttpEntity<?> entity = new HttpEntity<>(header);
 
             String url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst";
-            UriComponents uri = UriComponentsBuilder.fromHttpUrl(url+"?"+"serviceKey=%2FFMFpVx%2Bvq%2BeEKK6owg7QDf44oVwyYvRL8I8DAo1ZgHpJmeQ0YXKYlelKIafadOJJ5nB%2B1OV1l5pJyKwMuI3Pg%3D%3D&numOfRows=10&pageNo=1&sidoName=%EC%84%9C%EC%9A%B8&searchCondition=DAILY&_returnType=json").build();
+            String serviceKey = "P4zgBHeiRu9%2B499IV12fo7S463t6lMsVwSGZqk1mVWYVzNnpcGrp8MMCwZg2R6cmBiT4eSqmdj%2BrnAl59wkdJg%3D%3D";
+            String decodeServiceKey = URLDecoder.decode(serviceKey, "UTF-8");
 
-            //API 호출
-            ResponseEntity<Map> resultMap = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Map.class);
-            result.put("statusCode", resultMap.getStatusCodeValue());
-            result.put("header", resultMap.getHeaders()); //헤더 정보 확인
-            result.put("body", resultMap.getBody()); //실제 데이터 정보 확인
+            UriComponents builder = UriComponentsBuilder.fromHttpUrl(url)
+                .queryParam("serviceKey", decodeServiceKey)
+                .queryParam("sidoName", "서울")
+                .queryParam("searchCondition", "DAILY")
+                .queryParam("_returnType", "json")
+                .build(false);
 
-            //string형태로 파싱해줌
-            ObjectMapper mapper = new ObjectMapper();
-            jsonInString = mapper.writeValueAsString(resultMap.getBody());
-        return jsonInString;
+            Object response = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, new HttpEntity<String>(header), String.class);
+        return response;
     }
 }
 
