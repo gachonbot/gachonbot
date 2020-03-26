8a7d47bd81f43b8815f3bf01ac887e8d89172f50
Somin <qkrthals0524@naver.com>
2020-03-26 23:23:24 +0900

    MaskTestCode

--- src/main/java/com/bot/gachon/controller/GachonController.java
+++ src/main/java/com/bot/gachon/controller/GachonController.java
@@ -8,10 +8,7 @@ import com.bot.gachon.dto.res.HaksikDto;
 import com.bot.gachon.dto.res.TextReplyResponse;
 import com.bot.gachon.dto.res.WeatherDto;
 import com.bot.gachon.service.GachonService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -42,7 +39,7 @@ public class GachonController {
 //    public List<GachonMask> getMaskInfo() {
 //        return gachonService.findMaskInfo();
 //    }
-    @GetMapping("/mask")
+    @PostMapping("/mask")
     public TextReplyResponse getMaskInfo() {
         return gachonService.findMaskInfo();
     }
