2be8f4eb62bc7b7d2e77caca994757ae36e9f564
zzaro <59715304+zzaro@users.noreply.github.com>
2020-03-30 21:16:49 +0900

    JpaRepository 구현

--- src/main/java/com/bot/gachon/controller/GachonController.java
+++ src/main/java/com/bot/gachon/controller/GachonController.java
@@ -1,28 +1,30 @@
 package com.bot.gachon.controller;
 
-import com.bot.gachon.domain.GachonMask;
-import com.bot.gachon.domain.GachonYesterdayMask;
 import com.bot.gachon.dto.response.DustModel;
+import com.bot.gachon.dto.response.DustRepository;
 import com.bot.gachon.dto.response.HaksikDto;
 import com.bot.gachon.dto.response.WeatherDto;
 import com.bot.gachon.service.GachonService;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.List;
+
 
 @RestController
 public class GachonController {
 
+
     private final GachonService gachonService;
+    private final DustRepository dustRepository;
 
-    public GachonController(GachonService gachonService) {
-
+    public GachonController(GachonService gachonService, DustRepository dustRepository) {
         this.gachonService = gachonService;
-
+        this.dustRepository = dustRepository;
     }
+
 
     @GetMapping("/weather")
     public WeatherDto getWeatherInfo() throws Exception {
@@ -30,35 +32,18 @@ public class GachonController {
 
     }
 
-    @GetMapping("/haksik/building")
-    public HaksikDto getHaksikInfo(@RequestParam("building") String building) throws IOException {
-        return gachonService.findHaksikInfo(building);
-    }
-
-    @GetMapping("/mask")
-    public List<GachonMask> getMaskInfo() {
-        return gachonService.findMaskInfo();
-    }
-
-    @GetMapping("/remain/mask")
-    public List<GachonMask> getRemainMaskInfo() {
-        return gachonService.getRemainMaskInfo();
-    }
-
-    @GetMapping("/yesterday/mask")
-    public  List<GachonYesterdayMask> getYesterdatInfo(){ return gachonService.findYesterdayMaskInfo();
+    @GetMapping("/haksik")
+    public HaksikDto getHaksikInfo() throws IOException {
+         return gachonService.findHaksikInfo();
     }
 
     @GetMapping("/dust")
     public DustModel getDust() throws IOException {
+        dustRepository.save
         return gachonService.findDust();
     }
 
-
-
-
 }
-
 
 
 
