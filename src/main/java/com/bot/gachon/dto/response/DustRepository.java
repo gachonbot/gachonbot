4e7cd1aa8d529d75830a49ec06b47a4e7cd749f9
zzaro <59715304+zzaro@users.noreply.github.com>
2020-03-30 21:20:14 +0900

    Update DustRepository.java

--- src/main/java/com/bot/gachon/dto/response/DustRepository.java
+++ src/main/java/com/bot/gachon/dto/response/DustRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface DustRepository extends JpaRepository<Dust, Long> {
+public interface DustRepository extends JpaRepository<Dust, String> {
 
 }
