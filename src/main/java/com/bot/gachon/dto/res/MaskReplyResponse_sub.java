3d6c8e5922aa9c7c9c71f02fd77ba894e7dfbd29
Somin <qkrthals0524@naver.com>
2020-03-31 19:19:52 +0900

    MaskTestCode

--- src/main/java/com/bot/gachon/dto/res/MaskReplyResponse_sub.java
+++ src/main/java/com/bot/gachon/dto/res/MaskReplyResponse_sub.java
@@ -9,15 +9,13 @@ import java.util.HashMap;
 @Getter
 public class MaskReplyResponse_sub {
 
-    private ArrayList<HashMap<String,Object>> items = new ArrayList<>();
+    private HashMap<String,Object> item = new HashMap<>();
 
     public MaskReplyResponse_sub(){}
 
     @Builder
     public MaskReplyResponse_sub(String label, String blockId){
-        HashMap<String, Object> item = new HashMap<>();
         item.put("title", "마스크재고");
-
         HashMap<String, String> thumbnailChild = new HashMap<>();
         thumbnailChild.put("imageUrl", "");
         item.put("thumbnail", thumbnailChild);
@@ -26,12 +24,9 @@ public class MaskReplyResponse_sub {
         buttonsChild.put("action","block");
         buttonsChild.put("label", label);
         buttonsChild.put("blockId",blockId);
-
         buttonsParent.add(buttonsChild);
         item.put("buttons",buttonsParent);
-
     }
-
-    }
+}
 
 
