f3f40181544f5ce434f83943c83826c2ac44075d
Somin <qkrthals0524@naver.com>
2020-04-07 21:46:39 +0900

    MaskTestCode

--- src/main/java/com/bot/gachon/service/Url.java
+++ src/main/java/com/bot/gachon/service/Url.java
@@ -5,6 +5,6 @@ public class Url {
     public static final String HAKSIK_URL_ART ="http://m.gachon.ac.kr/menu/menu.jsp?gubun=A";
     public static final String HAKSIK_URL_EDU ="http://m.gachon.ac.kr/menu/menu.jsp?gubun=B";
     public static final String HAKSIK_URL_VISION = "http://m.gachon.ac.kr/menu/menu.jsp?gubun=C";
-    public static final String MASK_URL = "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json?lat=37.450694&lng=127.128638&m=1500";
+    public static final String MASK_URL = "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json?lat=37.450694&lng=127.128638&m=1000";
     public static final String DUST_URL = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey=P4zgBHeiRu9%2B499IV12fo7S463t6lMsVwSGZqk1mVWYVzNnpcGrp8MMCwZg2R6cmBiT4eSqmdj%2BrnAl59wkdJg%3D%3D&numOfRows=10&pageNo=1&stationName=%EB%B3%B5%EC%A0%95%EB%8F%99&dataTerm=DAILY&ver=1.3&_returnType=json";
 }
