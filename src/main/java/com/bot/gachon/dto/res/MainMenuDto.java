4b275d715d6287356758ef6032065412b450633f
Somin <qkrthals0524@naver.com>
2020-04-15 01:03:07 +0900

    공지사항기능구현

--- src/main/java/com/bot/gachon/dto/res/MainMenuDto.java
+++ src/main/java/com/bot/gachon/dto/res/MainMenuDto.java
@@ -15,34 +15,128 @@ public class MainMenuDto {
     public MainMenuDto(){
         this.version = "2.0";
 
-        HashMap<String,String> thumbnail = new HashMap<>();
-        thumbnail.put("imageUrl","https://hswsns.s3.ap-northeast-2.amazonaws.com/img/portfolio/mask4.jpg");
-        HashMap<String,Object> basicCard = new HashMap<>();
-        basicCard.put("thumbnail",thumbnail);
+
+
+
         HashMap<String,String> simpleText = new HashMap<>();
         simpleText.put("text","안녕하세요! 가천봇입니다.");
         ArrayList<HashMap<String,String>> buttonsParent = new ArrayList<>();
+
         HashMap<String,String> buttonsChild1 = new HashMap<>();
         buttonsChild1.put("action","message");
-        buttonsChild1.put("label", "공지사항");
-        buttonsChild1.put("messageText","공지사항");
-        buttonsParent.add(buttonsChild1);
+        buttonsChild1.put("label", "학사메뉴");
+        buttonsChild1.put("messageText","학사메뉴");
+
 
         HashMap<String,String> buttonsChild2 = new HashMap<>();
         buttonsChild2.put("action","message");
-        buttonsChild2.put("label", "무당이");
-        buttonsChild2.put("messageText","무당이");
+        buttonsChild2.put("label", "공지사항");
+        buttonsChild2.put("messageText","공지사항");
 
+        HashMap<String,String> buttonsChild3 = new HashMap<>();
+        buttonsChild2.put("action","message");
+        buttonsChild2.put("label", "다음 무당이?");
+        buttonsChild2.put("messageText","다음 무당이?");
+
+
+        buttonsParent.add(buttonsChild1);
         buttonsParent.add(buttonsChild2);
-        basicCard.put("buttons",buttonsParent);
+        buttonsParent.add(buttonsChild3);
+
+
+
+
+        HashMap<String,String> thumbnail = new HashMap<>();
+        thumbnail.put("imageUrl","https://hswsns.s3.ap-northeast-2.amazonaws.com/img/portfolio/mask4.jpg");
+
+        HashMap<String,String> thumbnail2 = new HashMap<>();
+        thumbnail.put("imageUrl","https://hswsns.s3.ap-northeast-2.amazonaws.com/img/portfolio/mask4.jpg");
+
+        HashMap<String,String> thumbnail3 = new HashMap<>();
+        thumbnail.put("imageUrl","https://hswsns.s3.ap-northeast-2.amazonaws.com/img/portfolio/mask4.jpg");
+
+
+        ArrayList<HashMap<String,String>> buttonsParent2 = new ArrayList<>();
+
+        HashMap<String,String> buttonsChild4 = new HashMap<>();
+        buttonsChild1.put("action","message");
+        buttonsChild1.put("label", "실시간 날씨");
+        buttonsChild1.put("messageText","실시간 날씨");
+
+
+        HashMap<String,String> buttonsChild5 = new HashMap<>();
+        buttonsChild2.put("action","message");
+        buttonsChild2.put("label", "실시간 미세먼지");
+        buttonsChild2.put("messageText","실시간 미세먼지");
+
+        HashMap<String,String> buttonsChild6= new HashMap<>();
+        buttonsChild2.put("action","message");
+        buttonsChild2.put("label", "도서관 자리현황");
+        buttonsChild2.put("messageText","도서관 자리현황");
+
+        ArrayList<HashMap<String,String>> buttonsParent3 = new ArrayList<>();
+
+        HashMap<String,String> buttonsChild7 = new HashMap<>();
+        buttonsChild1.put("action","message");
+        buttonsChild1.put("label", "맛집 메뉴");
+        buttonsChild1.put("messageText","맛집 메뉴");
+
+
+        HashMap<String,String> buttonsChild8 = new HashMap<>();
+        buttonsChild2.put("action","message");
+        buttonsChild2.put("label", "오늘 뭐 먹지?");
+        buttonsChild2.put("messageText","오늘 뭐 먹지?");
+
+        HashMap<String,String> buttonsChild9= new HashMap<>();
+        buttonsChild2.put("action","message");
+        buttonsChild2.put("label", "오늘의 식단(학식)");
+        buttonsChild2.put("messageText","오늘의 식단(학식)");
+
+
+        buttonsParent2.add(buttonsChild4);
+        buttonsParent2.add(buttonsChild5);
+        buttonsParent2.add(buttonsChild6);
+
+        buttonsParent3.add(buttonsChild7);
+        buttonsParent3.add(buttonsChild8);
+        buttonsParent3.add(buttonsChild9);
+
+
+
+
+        ArrayList<HashMap<String,Object>> items = new ArrayList<>();
+
+       HashMap<String,Object> item = new HashMap<>();
+       item.put("thumbnail",thumbnail);
+       item.put("buttons",buttonsParent);
+       items.add(item);
+
+        HashMap<String,Object> item2 = new HashMap<>();
+        item2.put("thumbnail",thumbnail2);
+        item2.put("buttons",buttonsParent2);
+        items.add(item2);
+        HashMap<String,Object> item3 = new HashMap<>();
+        item3.put("thumbnail",thumbnail3);
+        item3.put("buttons",buttonsParent3);
+        items.add(item3);
 
         ArrayList<HashMap<String,Object>> outputs = new ArrayList<>();
+
         HashMap<String,Object> output = new HashMap<>();
         output.put("simpleText",simpleText);
-        HashMap<String,Object> output2 = new HashMap<>();
-        output2.put("basicCard",basicCard);
-        outputs.add(output);
+
+
+        HashMap<String,Object>output2 = new HashMap<>();
+
+        HashMap<String,Object> carousel = new HashMap<>();
+
+        carousel.put("type","basicCard");
+        carousel.put("items",items);
+        output2.put("carousel",carousel);
         outputs.add(output2);
+
+
+
         this.template.put("outputs",outputs);
 
 
