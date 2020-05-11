431a14467cd60b975095c85d548bb78e87b5b59f
zzaro <59715304+zzaro@users.noreply.github.com>
2020-05-11 21:13:44 +0900

    Add files via upload

---
+++ src/main/java/com/bot/gachon/domain/DustRepository.java
@@ -0,0 +1,15 @@
+package com.bot.gachon.domain;
+
+        import org.springframework.data.jpa.repository.JpaRepository;
+        import org.springframework.data.jpa.repository.Query;
+        import org.springframework.stereotype.Repository;
+
+        import java.util.List;
+        import java.util.Optional;
+@Repository
+public interface DustRepository extends JpaRepository<GachonDust, String> {
+    @Query("SELECT p FROM GachonDust p ORDER BY p.dataTime")
+    List<GachonDust> findAll();
+
+    Optional<List<GachonDust>> findAllByPm10Grade1h(String pm10Grade1h);
+}
