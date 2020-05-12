eb4acc789b5c790dab22e9b410fd117022cac642
somin <qkrthals0524@naver.com>
2020-05-12 15:45:44 +0900

    학식조회테스트

--- src/main/java/com/bot/gachon/service/HaksikUrl.java
+++ src/main/java/com/bot/gachon/service/HaksikUrl.java
@@ -1,7 +1,7 @@
 package com.bot.gachon.service;
 
 public enum  HaksikUrl {
-    vision(Url.HAKSIK_URL_ART), art(Url.HAKSIK_URL_VISION), edu(Url.HAKSIK_URL_EDU);
+    vision(Url.HAKSIK_URL_VISION), art(Url.HAKSIK_URL_ART), edu(Url.HAKSIK_URL_EDU);
     public String link = "";
 
     HaksikUrl(String link) {
