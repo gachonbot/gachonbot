af1aa300ad4118962029291999397e662e5ee565
somin <qkrthals0524@naver.com>
2020-06-01 23:02:40 +0900

    Month선택 테스트

--- src/main/java/com/bot/gachon/service/Schedule.java
+++ src/main/java/com/bot/gachon/service/Schedule.java
@@ -35,6 +35,8 @@ public class Schedule {
             if ((child.getElementsByTag("a").text()).equals(dayofMonth)) {
                 for(Element child2 : child.getElementsByTag("div").get(0).getElementsByTag("dl")){
                     content+=child2.getElementsByTag("dd").text() + "\n" + child2.getElementsByTag("dt").text();
+                    System.out.println("# somini1 = " + child2.getElementsByTag("dd").text());
+                    System.out.println("# somini2 = " + child2.getElementsByTag("dt").text());
                 }
             }
         }
